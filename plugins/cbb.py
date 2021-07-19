c6d3b1ec079cd2e9446c8e7f49f4e061f925479d
Kuttu22 <86157396+Kuttu22@users.noreply.github.com>
2021-07-19 09:37:27 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Master : <a href='https://t.me/COLD_ONEz'>COLD_ONEz</a>\n<b>○ Blank : <a href='https://t.me/Fantasy_Worldz'>FANTASY_WORLD</a>\n<b>○ Group : <a href='https://t.me/Fantasy_Movies'>FANTASY_MOVIES</a>\n<b>○ Channel : <a href='https://t.me/Fantasy_Seriesz'>FANTASY_SERIES</a></b>",
+            text = f"<b>○ Master : <a href='https://t.me/COLD_ONEz'>COLD_ONEz</a>\n<b>○ Blank : <a href='https://t.me/hhhhdssbh'>Test channel fw</a>\n<b>○ Group : <a href='https://t.me/kuttufw'>Fw and cinema kotta</a>\n<b>○ Channel : <a href='https://t.me/akmvz001'>Filmworld movies</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
